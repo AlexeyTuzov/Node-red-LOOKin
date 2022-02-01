66b3dc9dd39c71bcdd06ded50037facfa8263c0e
AresNexus <aresnexus@rambler.ru>
2022-02-01 15:48:50 +0500

    Added labelsand default obj

--- src/master/master.ts
+++ src/master/master.ts
@@ -4,7 +4,13 @@ export = function (RED: nodeRed.NodeAPI): void {
     RED.nodes.registerType('master', function (this: nodeRed.Node, config: nodeRed.NodeDef) {
         RED.nodes.createNode(this, config);
         let context = this.context().global;
+        this.name = config.name;
         this.on('close', function () {
+        });
+        this.on('input', function(msg, send, done) {
+            let message: nodeRed.NodeMessage = {payload: this.name}
+            send(message);
+            done();
         });
     });
 };
