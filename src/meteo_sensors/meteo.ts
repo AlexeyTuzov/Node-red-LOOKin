d193b76b5416b84224d398e3794065ed398e8539
AresNexus <aresnexus@rambler.ru>
2022-03-22 14:49:17 +0500

    meteo sensors event listener has been added

--- src/meteo_sensors/meteo.ts
+++ src/meteo_sensors/meteo.ts
@@ -2,6 +2,8 @@ import * as nodeRed from 'node-red';
 import { INode, IConfig } from '../_utilites/NodeRedUtilites/common node/nodeInterfaces';
 import masterEmitter from '../_utilites/NodeRedUtilites/common node/masterEventEmitter';
 import { Device } from '../_utilites/interfaces';
+import initializeMeteo from '../_utilites/NodeRedUtilites/meteo/initializeMeteo';
+import { emitter } from '../_utilites/UDPserver';
 
 export = function (RED: nodeRed.NodeAPI) {
     RED.nodes.registerType ('meteo', function (this: INode, config: IConfig) {
@@ -13,6 +15,18 @@ export = function (RED: nodeRed.NodeAPI) {
 
         masterEmitter.on('initialized', async () => {
             let device : Device | any = context.get('deviceInfo');
+            await initializeMeteo(this, device);
+        });
+
+        const METEO_UPDATE_EXPRESSION: string = String.raw`LOOK\.?in:Updated!${this.ID}:FE:00:\w{8}`;
+        emitter.on('updated_meteo', (msg: string) => {
+            if (msg.match(RegExp(METEO_UPDATE_EXPRESSION))) {
+                let measuredTemp: number = parseInt(msg.slice(-8, -4), 16) / 10;
+                let measuredHumidity: number = parseInt(msg.slice(-4), 16) / 10;
+                let msgTemp: nodeRed.NodeMessage = {payload: `Temperature: ${measuredTemp} \u00B0C` };
+                let msgHumidity: nodeRed.NodeMessage = {payload: `Humidity: ${measuredHumidity} %`};
+                this.send([msgTemp, msgHumidity]);
+            }
         });
     });
 }
